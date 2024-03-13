caf93245b661c21172b0d90345b4cc42ada7c2aa
LolaMelnychuk <errton@gmail.com>
2024-03-13 18:53:02 +0200

    lr3 is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { GreetComponent } from './greet/greet.component';
 import { StudentComponent } from './student/student.component';
-
 
  
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [GreetComponent, StudentComponent],
+  imports: [StudentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
   export class AppComponent {}
+  export class YourModule { }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-student',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
@@ -11,12 +11,31 @@ export class StudentComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  name: string = "Lola Melnychuk";
-  age: number = 20;
-  mobile: string = "068299950";
-  department: string = "ITRE, TR";
 
   constructor() { }
+  students: any[] = [
+    {
+    ID: 'std101', FirstName: 'Jane', LastName: 'Tiwars',
+    Branch: 'CSE', DOB: '29/02/1988', Gender: 'Female'
+    },
+    {
+    ID: 'std102', FirstName: 'Andy', LastName: 'Mohan', 
+    Branch: 'ETC', DOB: '23/05/1989', Gender: 'Male'
+    },
+    {
+    ID: 'std103', FirstName: 'Patricia', LastName: 'Dungan', 
+    Branch: 'CSE', DOB: '24/07/1992', Gender: 'Female'
+    },
+    {
+    ID: 'std104', FirstName: 'Nina', LastName: 'Sharp', 
+    Branch: 'ETC', DOB: '19/08/1990', Gender: 'Female'
+    },
+    {
+    ID: 'std105', FirstName: 'Samuel', LastName: 'Stone', 
+    Branch: 'CSE', DOB: '12/94/1991', Gender: 'Male'
+    }
+    ];
+    }
+    
 
 
-}
